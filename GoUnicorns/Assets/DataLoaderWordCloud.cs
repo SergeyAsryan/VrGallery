7681f9254b87cfc6c8de7c6f02de54673fa8b155
b3nn <benn@verybadposture.com>
2016-05-15 21:12:03 -0400

    Google hot trends word cloud. Pulls top 20 words from the USA (updated hourly I think)

---
+++ GoUnicorns/Assets/DataLoaderWordCloud.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using SimpleJSON;
+
+public class DataLoaderWordCloud : MonoBehaviour {
+
+    // Array of items, pull the "aws_url" for direct link to image
+    public string url = "https://ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=10&q=http://www.google.com/trends/hottrends/atom/feed?pn=p1";
+    public GameObject lightPreFab;
+    private int ballcount = 0;
+    private JSONNode json;
+
+    // Use this for initialization
+    IEnumerator Start()
+    {
+        WWW www = new WWW(url);
+        yield return www;
+        Debug.Log(www.text);
+        json = JSON.Parse(www.text);
+        InvokeRepeating("CreateLightBall", 0, 3.0f);
+    }
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+
+    void CreateLightBall()
+    {
+        ballcount++;
+        string ballname = "test-" + ballcount;
+        ballname = json[ballcount]["digitalid"].Value;
+
+
+        Debug.Log("Making Light Ball:" + ballname);
+        Transform cameraTransform = Camera.main.gameObject.transform;
+        Vector3 ballVector = cameraTransform.position + new Vector3(1, 1, 6);
+        GameObject lightBall = Instantiate(lightPreFab, ballVector, cameraTransform.rotation) as GameObject;
+        GameObject ballText = lightBall.transform.Find("BallText").gameObject;
+        ballText.GetComponent<TextMesh>().text = ballname;
+
+    }
+}
--- GoUnicorns/Assets/FormWordCloud.cs
+++ GoUnicorns/Assets/FormWordCloud.cs
@@ -1,14 +1,35 @@
 ﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+using LitJson;
+
+public class Phrase
+{
+    public string term;
+    public float occurrences;
+}
 
 public class FormWordCloud : MonoBehaviour
 {
     public GameObject childObject;
     public float size = 10.0f;
-    public float objects = 10.0f;
+    private List<Phrase> phrases = new List<Phrase>();
 
-    void Start()
+    public string urlGoogle = "https://ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=20&q=http://www.google.com/trends/hottrends/atom/feed?pn=p1";
+    private string jsonGoogle = "{\"responseData\": {\"feed\":{\"feedUrl\":\"http://www.google.com/trends/hottrends/atom/feed?pn\u003dp1\",\"title\":\"Hot Trends\",\"link\":\"http://www.google.com/trends/hottrends?pn\u003dp1\",\"author\":\"\",\"description\":\"Recent hot searches\",\"type\":\"rss20\",\"entries\":[{\"title\":\"Anthony Kiedis\",\"link\":\"http://www.google.com/trends/hottrends?pn\u003dp1#a\u003d20160515-Anthony+Kiedis\",\"author\":\"\",\"publishedDate\":\"Sun, 15 May 2016 04:00:00 -0700\",\"contentSnippet\":\"Red Hot Chili Peppers\",\"content\":\"Red Hot Chili Peppers\",\"categories\":[]},{\"title\":\"Manchester United\",\"link\":\"http://www.google.com/trends/hottrends?pn\u003dp1#a\u003d20160515-Manchester+United\",\"author\":\"\",\"publishedDate\":\"Sun, 15 May 2016 08:00:00 -0700\",\"contentSnippet\":\"Old Trafford\",\"content\":\"Old Trafford\",\"categories\":[]},{\"title\":\"Bay to Breakers 2016\",\"link\":\"http://www.google.com/trends/hottrends?pn\u003dp1#a\u003d20160515-Bay+to+Breakers+2016\",\"author\":\"\",\"publishedDate\":\"Sun, 15 May 2016 08:00:00 -0700\",\"contentSnippet\":\"Bay to Breakers\",\"content\":\"Bay to Breakers\",\"categories\":[]},{\"title\":\"Pentecost\",\"link\":\"http://www.google.com/trends/hottrends?pn\u003dp1#a\u003d20160515-Pentecost\",\"author\":\"\",\"publishedDate\":\"Sun, 15 May 2016 04:00:00 -0700\",\"contentSnippet\":\"\",\"content\":\"\",\"categories\":[]},{\"title\":\"Formula 1\",\"link\":\"http://www.google.com/trends/hottrends?pn\u003dp1#a\u003d20160515-Formula+1\",\"author\":\"\",\"publishedDate\":\"Sun, 15 May 2016 10:00:00 -0700\",\"contentSnippet\":\"F1\",\"content\":\"F1\",\"categories\":[]},{\"title\":\"UFC 198\",\"link\":\"http://www.google.com/trends/hottrends?pn\u003dp1#a\u003d20160514-UFC+198\",\"author\":\"\",\"publishedDate\":\"Sat, 14 May 2016 10:00:00 -0700\",\"contentSnippet\":\"UFC, UFC 198 results, Stipe Miocic, Stipe Miocic Vs Fabricio Werdum, UFC 198 fight card, Werdum vs Miocic\",\"content\":\"UFC, UFC 198 results, Stipe Miocic, Stipe Miocic Vs Fabricio Werdum, UFC 198 fight card, Werdum vs Miocic\",\"categories\":[]},{\"title\":\"Barcelona\",\"link\":\"http://www.google.com/trends/hottrends?pn\u003dp1#a\u003d20160514-Barcelona\",\"author\":\"\",\"publishedDate\":\"Sat, 14 May 2016 09:00:00 -0700\",\"contentSnippet\":\"La Liga, FC Barcelona\",\"content\":\"La Liga, FC Barcelona\",\"categories\":[]},{\"title\":\"Eurovision 2016\",\"link\":\"http://www.google.com/trends/hottrends?pn\u003dp1#a\u003d20160514-Eurovision+2016\",\"author\":\"\",\"publishedDate\":\"Sat, 14 May 2016 13:00:00 -0700\",\"contentSnippet\":\"Eurovision\",\"content\":\"Eurovision\",\"categories\":[]},{\"title\":\"Rose Leslie\",\"link\":\"http://www.google.com/trends/hottrends?pn\u003dp1#a\u003d20160514-Rose+Leslie\",\"author\":\"\",\"publishedDate\":\"Sat, 14 May 2016 06:00:00 -0700\",\"contentSnippet\":\"Kit Harington\",\"content\":\"Kit Harington\",\"categories\":[]},{\"title\":\"OJ Simpson\",\"link\":\"http://www.google.com/trends/hottrends?pn\u003dp1#a\u003d20160514-OJ+Simpson\",\"author\":\"\",\"publishedDate\":\"Sat, 14 May 2016 01:00:00 -0700\",\"contentSnippet\":\"\",\"content\":\"\",\"categories\":[]}]}}, \"responseDetails\": null, \"responseStatus\": 200}";
+
+    private string jsonString = "[{\"term\":\"the\", \"occurrences\":504},{\"term\":\"to\",\"occurrences\":447},{\"term\":\"rt\",\"occurrences\":433},{\"term\":\"a\",\"occurrences\":382},{\"term\":\"in\",\"occurrences\":299},{\"term\":\"of\",\"occurrences\":274},{\"term\":\"adventure\",\"occurrences\":236},{\"term\":\"and\",\"occurrences\":216},{\"term\":\"for\",\"occurrences\":166},{\"term\":\"is\",\"occurrences\":157},{\"term\":\"on\",\"occurrences\":154},{\"term\":\"cars\",\"occurrences\":136},{\"term\":\"it\",\"occurrences\":122},{\"term\":\"you\",\"occurrences\":116},{\"term\":\"with\",\"occurrences\":100},{\"term\":\"from\",\"occurrences\":87},{\"term\":\"at\",\"occurrences\":85},{\"term\":\"i\",\"occurrences\":85},{\"term\":\"this\",\"occurrences\":85},{\"term\":\"that\",\"occurrences\":83}]";
+
+
+    // Use this for initialization
+    IEnumerator Start()
     {
-        Sphere(objects);
+        WWW www = new WWW(urlGoogle);
+        yield return www;
+        Debug.Log(www.text);
+        jsonGoogle = www.text;
+        loadGoogle();
+        Sphere();
     }
 
     // Update is called once per frame
@@ -24,8 +45,22 @@ public class FormWordCloud : MonoBehaviour
         }
     }
 
-    private void Sphere(float points)
+    private void loadGoogle()
     {
+        JsonData jsonvale = JsonMapper.ToObject(jsonGoogle);
+        JsonData entries = jsonvale["responseData"]["feed"]["entries"];
+        for (int i = 0; i < entries.Count; i++)
+        {
+            Phrase phrase = new Phrase();
+            phrase.term = entries[i]["title"].ToString();
+            phrase.occurrences = 1;
+            phrases.Add(phrase);
+        }
+    }
+
+    private void Sphere()
+    {
+        float points = phrases.Count;
         float increment = Mathf.PI * (3 - Mathf.Sqrt(5));
         float offset = 2 / points;
         for (float i = 0; i < points; i++)
@@ -38,6 +73,20 @@ public class FormWordCloud : MonoBehaviour
             // Create the object as a child of the sphere
             GameObject child = Instantiate(childObject, pos, Quaternion.identity) as GameObject;
             child.transform.parent = transform;
+            TextMesh phraseText = child.transform.GetComponent<TextMesh>();
+            phraseText.text = phrases[(int)i].term;
+        }
+    }
+
+    private void ProcessWords(string jsonString)
+    {
+        JsonData jsonvale = JsonMapper.ToObject(jsonString);
+        for (int i = 0; i < jsonvale.Count; i++)
+        {
+            Phrase phrase = new Phrase();
+            phrase.term = jsonvale[i]["term"].ToString();
+            phrase.occurrences = float.Parse(jsonvale[i]["occurrences"].ToString());
+            phrases.Add(phrase);
         }
     }
 }
